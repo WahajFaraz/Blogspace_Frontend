96c19390db057f8b23b34d570665a9e084153ded
WahajFaraz <wahajfaraz93@gmail.com>
2025-09-18 07:22:21 +0500

    Update config.js

--- client/src/lib/config.js
+++ client/src/lib/config.js
@@ -1,7 +1,7 @@
 // Determine the base URL based on the environment
 export const API_BASE_URL = import.meta.env.DEV
-  ? 'https://blogs-backend-ebon.vercel.app/'
-  : 'https://blogs-backend-ebon.vercel.app/api/v1';
+  ? 'https://blogs-backend-ebon.vercel.app'
+  : 'https://blogs-backend-ebon.vercel.app';
 
 export const config = {
   API_BASE_URL,
