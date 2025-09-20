1b621658b6c33ea2a00455c6e3337ee5dfa3f334
WahajFaraz <wahajfaraz93@gmail.com>
2025-09-20 14:40:08 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./global.css";
 
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster as Sonner } from "./components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
